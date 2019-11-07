eb8c41e4df6c53303d5e2daa0e52d1b3ebe8b248
Caleb Tipton <calebtipton@gmail.com>
2019-11-06 22:36:30 -0700

    Add today's expected high

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,8 +13,9 @@ const forecast = (latitude, longitude, callback) => {
     } else {
         const {temperature, precipProbability} = body.currently
         const summary = body.daily.data[0].summary
+        const tempHigh = body.daily.data[0].temperatureHigh
         
-        callback(undefined, summary + ' It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain.')
+        callback(undefined, ' --' + summary + ' -- Today\'s expected high is: ' + tempHigh + ' degrees -- It is currently ' + temperature + ' degrees out. -- There is a ' + precipProbability + '% chance of rain.')
     }
 })
 }
